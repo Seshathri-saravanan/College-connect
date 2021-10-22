17c7ef1122e7d56dceb57458e502e4c715247268
seshathri saravanan <seshathrisaravanan@gmail.com>
2021-10-22 17:00:46 +0530

    added cors headers in response

--- index.js
+++ index.js
@@ -19,8 +19,17 @@ const connect  = Mongoose.connect(uri,{useUnifiedTopology:true,useNewUrlParser:t
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
+//app.use(cors());
 app.use(cookieParser("sfihf"));
+function addHeaders(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:8080/',);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  console.log("setting acces con c header as true",req.headers,res.headers);
+  next();
+}
+app.use(addHeaders)
 app.use(userRouter);
 function auth(req,res,next){
   //console.log("signedcookies",req.signedCookies.user,req.headers.cookie)
